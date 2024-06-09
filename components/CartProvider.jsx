a420b0e4c4e245e77bb58d61e571df6f9e0e315e
Ariel Videla <ariel_videla@hotmail.com>
2024-06-09 17:02:41 -0300

    first commit

--- components/CartProvider.jsx
+++ components/CartProvider.jsx
@@ -8,8 +8,8 @@ const CartProvider = ({ children }) => {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY}
-      successUrl="http://localhost:3000/stripe/success"
-      cancelUrl="http://localhost:3000/stripe/error"
+      successUrl="https://bikezz-eta.vercel.app/stripe/success"
+      cancelUrl="https://bikezz-eta.vercel.app/stripe/error"
       language="en-US"
       currency="USD"
       billingAddressCollection={true}
